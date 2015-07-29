c602d085a648a83454a1d7e5de5c52ba7d608bd6
prassahu <prashantsahu312@gmail.com>
2015-07-29 13:47:23 +0530

    Test cases updated

--- src/test/java/com/ecom/mail/SendMailTest.java
+++ src/test/java/com/ecom/mail/SendMailTest.java
@@ -4,16 +4,43 @@ import static org.junit.Assert.assertTrue;
 
 import javax.mail.Address;
 import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SendMailTest {
 
+	private SendMail sendMail;
+
+	@Before
+	public void setUp() {
+		sendMail = new SendMail();
+	}
+
 	@Test
 	public void testSendMail() throws MessagingException {
-		SendMail classUnderTest = new SendMail();
-		Address[] toAddresses = { new InternetAddress("prashantsahu034@gmail.com")};
-		assertTrue(classUnderTest.sendMessage("Subject Testing", "Body content",toAddresses));
+		Address[] toAddresses = { new InternetAddress(
+				"prashantsahu034@gmail.com") };
+		assertTrue(sendMail.sendMessage("Subject Testing", "Body content",
+				toAddresses));
 	}
+
+	@Test(expected = AddressException.class)
+	public void testSendMailFail() throws MessagingException {
+		Address[] toAddresses = { new InternetAddress("@gmail.com") };
+		sendMail.sendMessage("Subject Testing", "Body content", toAddresses);
+	}
+
+	@Test
+	public void testMultiAddressSendMail() throws MessagingException {
+		Address[] toAddresses = {
+				new InternetAddress("prashantsahu034@gmail.com"),
+				new InternetAddress("prashantsahu312@gmail.com"),
+				new InternetAddress("prashant.sahu@globallogic.com") };
+		assertTrue(sendMail.sendMessage("Subject Testing", "Body content",
+				toAddresses));
+	}
+
 }
